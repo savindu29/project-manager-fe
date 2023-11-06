407234f10b6beddfb87a1d7980a552a6d0a37881
RoshenH <roshen.20210497@gmail.com>
2023-11-06 16:25:03 +0530

    update create

--- src/pages/people/create/add-employees.tsx
+++ src/pages/people/create/add-employees.tsx
@@ -60,7 +60,7 @@ export default function EmployeeCreateForm() {
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
               <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                First name
+               Name
               </label>
               <div className="mt-2">
                 <input
@@ -76,21 +76,7 @@ export default function EmployeeCreateForm() {
               </div>
             </div>
 
-            <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                Last name
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
-
-                />
-              </div>
-            </div>
+            
 
             <div className="sm:col-span-4">
               <label htmlFor="companyEmail" className="block text-sm font-medium leading-6 text-gray-900">
