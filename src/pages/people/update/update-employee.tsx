f5c2252cd792b216818ddc9701356c66a230b670
RoshenH <roshen.20210497@gmail.com>
2023-11-09 10:05:39 +0530

    update ui

--- src/pages/people/update/update-employee.tsx
+++ src/pages/people/update/update-employee.tsx
@@ -80,142 +80,163 @@ export default function EmployeeUpdateForm() {
 
     return (
         <div>
-            <h1 className="text-base font-semibold leading-7 text-gray-900">Update Responsible Person</h1><br></br>
+        <div className="space-y-12">
+       
+
+            <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900 text-lg">Update Inova Persons</h2>
+
+            <div className="mt-5">
             <label className="mt-1 text-sm leading-6 text-gray-600">Select a person: </label>
-            <select className="border-color-black" value={selectedName} onChange={handleSelectName}>
-                {persons.map((person) => (
-                    <option  value={person.name}>
-                        {person.name}
-                    </option>
-                ))}
-            </select>
+                <select className="border-color-black" value={selectedName} onChange={handleSelectName}>
+                    {persons.map((person) => (
+                        <option  value={person.name}>
+                            {person.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
-
-
-
-            <div className="sm:col-span-3 mb-4 mt-5">
-                <label htmlFor="first-name" className="block text-sm font-medium font-semibold leading-6 text-gray-900 mb-1">
-                    Name
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
                 </label>
                 <div className="mt-2">
                     <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        autoComplete="given-name"
-                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-red focus:ring-inset  sm:text-sm sm:leading-6 "
-                        value={selectedPerson ? selectedPerson.name : ''}
-                        onChange={(e) => setSelectedPerson(selectedPerson
-                            ? { ...selectedPerson, name: e.target.value }
-                            : null)}
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
+                    value={selectedPerson ? selectedPerson.name : ''}
+                    onChange={(e) => setSelectedPerson(selectedPerson
+                        ? { ...selectedPerson, name: e.target.value }
+                        : null)}
 
+                    
+                    
                     />
                 </div>
-            </div>
+                </div>
 
+                
 
-
-            <div className="sm:col-span-4 mb-4">
-                <label htmlFor="companyEmail" className="block text-sm font-medium font-semibold leading-6 text-gray-900">
-                    Company Email address
+                <div className="sm:col-span-3">
+                <label htmlFor="companyEmail" className="block text-sm font-medium leading-6 text-gray-900">
+                   Mobile Number
                 </label>
                 <div className="mt-2">
                     <input
-                        id="companyEmail"
-                        name="companyEmail"
-                        type="email"
-                        autoComplete="email"
-                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
-                        value={selectedPerson ? selectedPerson.companyEmail : ''}
-                        onChange={(e) => setSelectedPerson(selectedPerson
-                            ? { ...selectedPerson, companyEmail: e.target.value }
-                            : null)}
+                    id="companyEmail"
+                    name="companyEmail"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
+                    value={selectedPerson ? selectedPerson.mobile : ''}
+                    onChange={(e) => setSelectedPerson(selectedPerson
+                        ? { ...selectedPerson, mobile: e.target.value }
+                        : null)}
+
                     />
                 </div>
-            </div>
+                </div>
 
-            <div className="sm:col-span-4 mb-4">
-                <label htmlFor="personalEmail" className="block text-sm font-medium font-semibold leading-6 text-gray-900">
+                <div className="sm:col-span-4">
+                <label htmlFor="personalEmail" className="block text-sm font-medium leading-6 text-gray-900">
                     Personal Email address
                 </label>
                 <div className="mt-2">
                     <input
-                        id="personalEmail"
-                        name="personalEmail"
-                        type="email"
-                        autoComplete="email"
-                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
-                        value={selectedPerson ? selectedPerson.privateEmail : ''}
-                        onChange={(e) => setSelectedPerson(selectedPerson
-                            ? { ...selectedPerson, privateEmail: e.target.value }
-                            : null)}
+                    id="personalEmail"
+                    name="personalEmail"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
+                    value={selectedPerson ? selectedPerson.privateEmail : ''}
+                    onChange={(e) => setSelectedPerson(selectedPerson
+                                    ? { ...selectedPerson, privateEmail: e.target.value }
+                                    : null)}
                     />
                 </div>
-            </div>
+                </div>
 
-            <div className="sm:col-span-2 sm:col-start-1 mb-4">
-                <label htmlFor="mobile" className="block text-sm font-medium font-semibold leading-6 text-gray-900">
-                    Mobile Number
+                <div className="sm:col-span-4">
+                <label htmlFor="mobile" className="block text-sm font-medium leading-6 text-gray-900">
+                    Company Email Address
                 </label>
                 <div className="mt-2">
                     <input
-                        type="text"
-                        name="mobile"
-                        id="mobile"
-                        autoComplete="tel"
-                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
-                        value={selectedPerson ? selectedPerson.mobile : ''}
-                        onChange={(e) => setSelectedPerson(selectedPerson
-                            ? { ...selectedPerson, mobile: e.target.value }
-                            : null)}
+                    type="text"
+                    name="mobile"
+                    id="mobile"
+                    autoComplete="tel"
+                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
+                    value={selectedPerson ? selectedPerson.companyEmail : ''}
+                    onChange={(e) => setSelectedPerson(selectedPerson
+                                    ? { ...selectedPerson, companyEmail: e.target.value }
+                                    : null)}
                     />
+                    
                 </div>
-            </div>
+                </div>
 
-            <div className="sm:col-span-2 mb-4">
-                <label htmlFor="designation" className="block text-sm font-medium font-semibold leading-6 text-gray-900">
+                <div className="sm:col-span-3 sm:col-start-1">
+                <label htmlFor="designation" className="block text-sm font-medium leading-6 text-gray-900">
                     Designation
                 </label>
                 <div className="mt-2">
                     <input
-                        type="text"
-                        name="designation"
-                        id="designation"
-                        autoComplete="designation"
-                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
-                        value={selectedPerson ? selectedPerson.designation : ''}
-                        onChange={(e) => setSelectedPerson(selectedPerson
-                            ? { ...selectedPerson, designation: e.target.value }
-                            : null)} />
+                    type="text"
+                    name="designation"
+                    id="designation"
+                    autoComplete="designation"
+                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
+                    value={selectedPerson ? selectedPerson.designation : ''}
+                    onChange={(e) => setSelectedPerson(selectedPerson
+                                    ? { ...selectedPerson, designation: e.target.value }
+                                    : null)}
+                    />
                 </div>
-            </div>
+                </div>
 
-            <div className="col-span-full mb-4">
-                <label htmlFor="skills" className="block text-sm font-medium font-semibold leading-6 text-gray-900">
+                <div className="sm:col-span-3">
+                <label htmlFor="skills" className="block text-sm font-medium leading-6 text-gray-900">
                     Specialized Areas
                 </label>
                 <div className="mt-2">
                     <input
-                        type="text"
-                        name="skills"
-                        id="skills"
-                        autoComplete="skills"
-                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
-                        value={selectedPerson ? selectedPerson.specializedField : ''}
-                        onChange={(e) => setSelectedPerson(selectedPerson
-                            ? { ...selectedPerson, specializedField: e.target.value }
-                            : null)}
+                    type="text"
+                    name="skills"
+                    id="skills"
+                    autoComplete="skills"
+                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6"
+                    value={selectedPerson ? selectedPerson.specializedField : ''}
+                    onChange={(e) => setSelectedPerson(selectedPerson
+                        ? { ...selectedPerson, specializedField: e.target.value }
+                        : null)}
+
                     />
                 </div>
-            <button
-                onClick={handleUpdateDetails}
-                className="rounded-md bg-sky-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-5"
+                </div>
+            </div>
+            </div>
 
+        
+        </div>
+
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+            onClick={handleUpdateDetails}
+            
+            className="rounded-md bg-sky-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-                Update Details
+            Update
             </button>
-        </div>
-        </div>
+      </div>
+
+     
+      </div>
 
     )
 }
