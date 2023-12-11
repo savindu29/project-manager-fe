4f2ef342efb40a63d296d78fba840917c87b0de2
Hashimi <COHDSE221F-043@student.nibm.lk>
2023-12-11 11:21:39 +0530

    Login Error

--- src/pages/Login/accRequest.tsx
+++ src/pages/Login/accRequest.tsx
@@ -60,7 +60,7 @@ const AccRequest: React.FC = () => {
       }
   };
   
-  
+
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
       <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
