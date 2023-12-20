0a76ca3cb3df32de83c04035dfe1237385a129a4
Hashimi <COHDSE221F-043@student.nibm.lk>
2023-12-20 10:35:11 +0530

    new window

--- src/pages/resource-manager/Componants/NameWindow.tsx
+++ src/pages/resource-manager/Componants/NameWindow.tsx
@@ -33,118 +33,130 @@ interface NameWindowProps {
     return (
       <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center overflow-hidden">
         {/* Window Layout */}
-        <div className="bg-white w-full sm:w-3/4 md:w-5/6 lg:w-11/12 xl:w-3/4 h-full sm:h-5/6 md:h-5/6 lg:h-5/6 xl:h-5/6 p-16 md:p-20 lg:p-24 xl:p-28 mt-8 mb-8 ml-80 mr-4 rounded-lg">
-  
-          {/* Close button in the right top corner */}
-          <div className="text-right">
-            <button
-              className="text-blue-500 hover:underline cursor-pointer"
-              onClick={onClose}
-            >
-              Close
-            </button>
-          </div>
-  
-          {/* Header */}
-          <div className="text-2xl font-bold mb-4">{headerText}</div>
-    {/* Page Division */}
-    <div className="flex">
-      {/* Left Section */}
-      <div className="w-full md:w-2/3 overflow-y-auto pr-4">
-        {/* Table in Left Section */}
-        <table className="w-full border-collapse">
-          {/* Table Headers */}
-          <thead>
-            <tr className="bg-gray-200">
-              <th className="p-2">Project Names</th>
-              <th className="p-2">Status</th>
-              <th className="p-2">Allocate Date</th>
-              <th className="p-2">Release Date</th>
-              <th className="p-2">Percentage</th>
-            </tr>
-          </thead>
-          {/* Table Body */}
-          <tbody>
-            {/* Render table rows with actual data */}
-          </tbody>
-        </table>
-      </div>
+        <div className="relative bg-white w-full sm:w-3/4 md:w-5/6 lg:w-11/12 xl:w-3/4 h-full sm:h-5/6 md:h-5/6 lg:h-5/6 xl:h-5/6 p-8 md:p-12 lg:p-16 xl:p-20 mt-0 mb-8 ml-80 mr-4 rounded-lg">
 
-      {/* Right Section */}
-      <div className="w-full md:w-1/3 p-4">
-        {/* Information in Right Section */}
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2">Date From</label>
-          <div className="flex items-center">
-            <input
-              type="text"
-              name="dateFrom"
-              value={formData.dateFrom}
-              onChange={handleChange}
-              className="w-full p-2 border rounded"
-            />
-            {/* CalendarIcon placeholder */}
-            <span className="ml-2"></span>
-          </div>
-        </div>
-
-        {/* Repeat the above structure for other fields in the Right Section */}
-
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2">Date From</label>
-          <div className="flex items-center">
-            <input
-              type="text"
-              name="dateFrom"
-              value={formData.dateFrom}
-              onChange={handleChange}
-              className="w-full p-2 border rounded"
-            />
-            {/* CalendarIcon placeholder */}
-            <span className="ml-2"></span>
-          </div>
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2">Date From</label>
-          <div className="flex items-center">
-            <input
-              type="text"
-              name="dateFrom"
-              value={formData.dateFrom}
-              onChange={handleChange}
-              className="w-full p-2 border rounded"
-            />
-            {/* CalendarIcon placeholder */}
-            <span className="ml-2"></span>
-          </div>
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2">Date From</label>
-          <div className="flex items-center">
-            <input
-              type="text"
-              name="dateFrom"
-              value={formData.dateFrom}
-              onChange={handleChange}
-              className="w-full p-2 border rounded"
-            />
-            {/* CalendarIcon placeholder */}
-            <span className="ml-2"></span>
-          </div>
-        </div>
-
-        {/* Button in Right Section */}
-        <button
-          className="bg-blue-500 text-white p-2 rounded"
-          onClick={handleSendRequest}
-        >
-          Send Request
-        </button>
-      </div>
-    </div>
-  </div>
+{/* Close button in the right top corner */}
+<div className="absolute top-0 right-0 mt-2 mr-2">
+  <button
+    className="text-blue-500 hover:underline cursor-pointer"
+    onClick={onClose}
+  >
+    Close
+  </button>
 </div>
-  );
+
+{/* Header */}
+<div className="text-2xl font-bold mb-2">
+  {headerText}
+</div>
+    
+          {/* Page Division */}
+          <div className="flex">
+            {/* Left Section */}
+            <div className="w-full md:w-2/3 overflow-y-auto pr-2 md:pr-4 border-r-2 border-gray-300">
+
+            <table className="w-full border-collapse mt-0">
+
+                {/* Table Headers */}
+                <thead>
+                  <tr className="bg-gray-200">
+                    <th className="p-2">Project Names</th>
+                    <th className="p-2">Status</th>
+                    <th className="p-2">Allocate Date</th>
+                    <th className="p-2">Release Date</th>
+                    <th className="p-2">Percentage</th>
+                  </tr>
+                </thead>
+                {/* Table Body */}
+                <tbody>
+                  {/* Render table rows with actual data */}
+                </tbody>
+              </table>
+            </div>
+    
+            {/* Right Section */}
+            <div className="w-full md:w-1/3 p-2 md:pl-4">
+              {/* Information in Right Section */}
+              <div className="mb-2">
+                <label className="block text-sm font-semibold mb-1">Date From</label>
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    name="dateFrom"
+                    value={formData.dateFrom}
+                    onChange={handleChange}
+                    className="w-full p-1 border rounded"
+                  />
+                  {/* CalendarIcon placeholder */}
+                  <span className="ml-1"></span>
+                </div>
+              </div>
+    
+              {/* Repeat the above structure for other fields in the Right Section */}
+              <div className="mb-2">
+                <label className="block text-sm font-semibold mb-1">Date to</label>
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    name="dateFrom"
+                    value={formData.dateFrom}
+                    onChange={handleChange}
+                    className="w-full p-1 border rounded"
+                  />
+                  {/* CalendarIcon placeholder */}
+                  <span className="ml-1"></span>
+                </div>
+              </div>
+              <div className="mb-2">
+                <label className="block text-sm font-semibold mb-1">Presentage</label>
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    name="dateFrom"
+                    value={formData.dateFrom}
+                    onChange={handleChange}
+                    className="w-full p-1 border rounded"
+                  />
+                </div>
+              </div>
+              <div className="mb-2">
+                <label className="block text-sm font-semibold mb-1">Role</label>
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    name="dateFrom"
+                    value={formData.dateFrom}
+                    onChange={handleChange}
+                    className="w-full p-1 border rounded"
+                  />
+                </div>
+              </div>
+              <div className="mb-2">
+                <label className="block text-sm font-semibold mb-1">Note</label>
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    name="dateFrom"
+                    value={formData.dateFrom}
+                    onChange={handleChange}
+                    className="w-full p-1 border rounded"
+                  />
+                </div>
+              </div>
+              <br></br>
+              {/* Button in Right Section */}
+              <button
+                className="bg-blue-500 text-white p-1 rounded"
+                onClick={handleSendRequest}
+              >
+                Send Request
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+    
 };
 
 export default NameWindow;
