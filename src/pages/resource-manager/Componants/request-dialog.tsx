935add7a49c96dff5beb80f34f33a8bed9b4e746
Hashimi <COHDSE221F-043@student.nibm.lk>
2023-12-27 20:47:16 +0530

    RequestDialog

--- src/pages/resource-manager/Componants/request-dialog.tsx
+++ src/pages/resource-manager/Componants/request-dialog.tsx
@@ -1,71 +1,140 @@
-// RequestDialog.tsx
-import { XMarkIcon, StopCircleIcon } from "@heroicons/react/20/solid";
-import React from "react";
-
+import React, { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/20/solid";
 
 interface RequestDialogProps {
   onClose: () => void;
   isOpen: boolean;
   checkedResourceNames: string[];
 }
+
 interface Project {
+  id: number;
   name: string;
-  status: string;
-  allocatedDate: string;
-  releaseDate: string;
-  percentage: number;
 }
 
-const resourcesAllocated: Project[] = [
+interface Resource {
+  name: string;
+  status: string;
+  allocatedProjects: Project[];
+  pendingProjects: Project[];
+}
+
+const resourcesAllocated: Resource[] = [
   {
-    name: "Project 1",
-    status: "Approved",
-    allocatedDate: "2023-02-15",
-    releaseDate: "2023-12-31",
-    percentage: 40,
+    name: "Resource1",
+    status: "Active",
+    allocatedProjects: [
+      { id: 1, name: "A Project " },
+      { id: 2, name: "B Project " },
+      { id: 3, name: "C Project " },
+    ],
+    pendingProjects: [
+      { id: 3, name: "Project C" },
+      { id: 4, name: "Project D" },
+      { id: 5, name: "Project B" },
+    ],
   },
   {
-    name: "Project 2",
-    status: "Pending",
-    allocatedDate: "2023-05-15",
-    releaseDate: "2023-10-31",
-    percentage: 60,
+    name: "Resource2",
+    status: "Inactive",
+    allocatedProjects: [
+      { id: 5, name: "Project E" },
+      { id: 6, name: "Project F" },
+    ],
+    pendingProjects: [
+      { id: 7, name: "Project G" },
+      { id: 8, name: "Project H" },
+    ],
   },
-  // Add more resource entities as needed
 ];
+interface AllocationDetails {
+  allocatedDate: Date;
+  releaseDate: Date;
+  contributionPercentage: number;
+}
 
-const RequestDialog: React.FC<RequestDialogProps> = ({ isOpen,onClose, checkedResourceNames }) => {
-  const [addedData, setAddedData] = useState<{
-    dateFrom: string;
-    dateTo: string;
-    percentage: string[];
-  }[]>([]);
-  
-  const [formData, setFormData] = useState({
-    dateFrom: "",
-    dateTo: "",
-    percentage: [] as string[],
-  });
+interface DetailedResource extends Resource {
+  details: AllocationDetails[];
+}
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
+const calculateContributionPercentage = (): number => {
+  // This is just a placeholder, you should replace it with your actual calculation logic
+  return Math.random() * 100;
+};
 
-  const handleAddData = () => {
-    setAddedData((prevData) => [...prevData, formData]);
-    setFormData({
-      dateFrom: "",
-      dateTo: "",
-      percentage: [] as string[],
+const getResourcesWithDetails = (resources: Resource[]): DetailedResource[] => {
+  return resources.map((resource) => {
+    const details: AllocationDetails[] = resource.allocatedProjects.map((project) => {
+      return {
+        allocatedDate: new Date(), // Replace with the actual allocated date
+        releaseDate: new Date(), // Replace with the actual release date
+        contributionPercentage: calculateContributionPercentage(),
+      };
     });
+
+    return {
+      ...resource,
+      details,
+    };
+  });
+};
+
+const detailedResources: DetailedResource[] = getResourcesWithDetails(resourcesAllocated);
+
+// Now you can access the details for each resource
+detailedResources.forEach((resource) => {
+  console.log(`Resource: ${resource.name}`);
+  resource.details.forEach((detail, index) => {
+    console.log(`  Project ${index + 1}:`);
+    console.log(`    Allocated Date: ${detail.allocatedDate}`);
+    console.log(`    Release Date: ${detail.releaseDate}`);
+    console.log(`    Contribution Percentage: ${detail.contributionPercentage}%`);
+  });
+  console.log();
+});
+
+
+const RequestDialog: React.FC<RequestDialogProps> = ({ isOpen, onClose, checkedResourceNames }) => {
+  const [selectedResource, setSelectedResource] = useState<DetailedResource | null>(null);
+
+  // Filter resources based on checked resource names
+  const filteredResources = resourcesAllocated.filter((resource) =>
+    checkedResourceNames.includes(resource.name)
+  );
+
+  const onResourceClick = (resource: Resource) => {
+    setSelectedResource(getResourcesWithDetails([resource])[0]);
   };
-  const handleClearData = () => {
-    setAddedData([]);
-  };
+  
+const getResourcesWithDetails = (resources: Resource[]): DetailedResource[] => {
+  return resources.map((resource) => {
+    const details: AllocationDetails[] = resource.allocatedProjects.map((project) => {
+      // Generate sample dates
+      const allocatedDate = new Date(); // Replace with the actual allocated date
+      const releaseDate = new Date(); // Replace with the actual release date
+
+      // Generate a random contribution percentage between 0 and 100
+      const contributionPercentage = Math.random() * 100;
+
+      return {
+        allocatedDate,
+        releaseDate,
+        contributionPercentage,
+      };
+    });
+
+    return {
+      ...resource,
+      details,
+    };
+  });
+};
+
+const detailedResources: DetailedResource[] = getResourcesWithDetails(resourcesAllocated);
+
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-end items-center py-10 pl-20 pr-4 z-50 ">
+    <div className={`fixed ${isOpen ? 'block' : 'hidden'} top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-end items-center py-10 pl-20 pr-4 z-50`}>
       <div className="bg-white p-6 rounded-md h-full w-5/6 relative">
         <button
           onClick={onClose}
@@ -73,116 +142,67 @@ const RequestDialog: React.FC<RequestDialogProps> = ({ isOpen,onClose, checkedRe
         >
           <XMarkIcon className="h-6 w-6" />
         </button>
-        <div className="w-full h-full flex ">
-          <div className="w-2/3 border-r border-zinc-300 px-4">
-            <h2 className="text-lg font-semibold mb-4"> {checkedResourceNames.length > 0
-                ? ` ${checkedResourceNames.join(', ')}`
-                : 'Resource Name'}
-            </h2>
-            <table className="min-w-full  table-auto">
-              <thead className="">
-                <tr className="text-zinc-400 font-normal text-left">
-                  <th className="p-2 font-normal text-sm">Project Name</th>
-                  <th className="p-2 font-normal text-sm">Status</th>
-                  <th className="p-2 font-normal text-sm">Allocated date</th>
-                <th className="p-2 font-normal text-sm">Relese Date</th>
-                <th className="p-2 font-normal text-sm">Perecentage</th>
-                </tr>
-              </thead>
-              <tbody className="border-y border-gray-300 text-sm">
-                {resourcesAllocated.map((project, index) => (
-                  <tr key={index}>
-                    <td className="border-b p-2 ">{project.name}</td>
-                    <td className="border-b p-2  ">
-                      <div
-                        className={
-                          " bg-green-700 flex text-white rounded py-1  w-28 pl-4 items-center text-xs"
-                        }
-                      >
-                        <CheckCircleIcon className="h-4 w-4 mr-2" />{" "}
-                        {project.status}
-                      </div>
-                    </td>
-                    <td className="border-b p-2">{project.allocatedDate}</td>
-                    <td className="border-b p-2">{project.releaseDate}</td>
-                    <td className="border-b p-2">{project.percentage}%</td>
-                  </tr>
-                  
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="w-1/3">
-  <div className="container mx-auto p-4">
-    <div className="flex flex-col space-y-4">
-      <div className="flex">
-        <div className="flex flex-col mr-4">
-          <label className="p-2 font-normal text-sm">Date From:</label>
-          <input
-            type="date"
-            name="dateFrom"
-            value={formData.dateFrom}
-            onChange={handleInputChange}
-            className="p-2 border rounded w-40"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label className="p-2 font-normal text-sm">Date To:</label>
-          <input
-            type="date"
-            name="dateTo"
-            value={formData.dateTo}
-            onChange={handleInputChange}
-            className="p-2 border rounded w-40"
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-col">
-        <label className="p-2 font-normal text-sm">Percentage:</label>
-        <input
-          type="number"
-          name="percentage"
-          value={formData.percentage}
-          onChange={handleInputChange}
-          placeholder="Percentage"
-          className="p-2 border rounded"
-        />
-      </div>
-
-      <div className="flex space-x-4 mt-4">
-      <button onClick={handleAddData} className="bg-green-500 flex text-white rounded py-1 px-3 justify-center items-center text-xs">
-  Add Data
-</button>
-<button onClick={handleClearData} className="bg-red-500 flex text-white rounded py-1 px-3 justify-center items-center text-xs">
-  Clear Data
-</button>
-
-      </div>
-    </div>
-
-    <div className="mt-4 overflow-y-auto max-h-48">
-  <h3 className="text-lg font-semibold mb-4">Added Requests:</h3>
-  <ul className="list-disc list-inside">
-    {addedData.map((data, index) => (
-      <li key={index} className="mb-2 text-sm text-zinc-500">
-        {`Date From: ${data.dateFrom}, Date To: ${data.dateTo}, Percentage: ${data.percentage}%`}
-      </li>
-    ))}
-  </ul>
-</div>
-
-
+        <div className="flex">
+          {/* Left Column (1/3 of the window) */}
+          <div className="w-1/3 h-full overflow-y-auto">
+          <div className="rounded-t-lg text-black text-2xl font-semibold py-2 px-4 mb-4"> 
+Current project detilas 
   </div>
+  {filteredResources.map((resource) => (
+    <div
+      key={resource.name}
+      className="h-24 w-full flex items-center cursor-pointer bg-zinc-200 hover:bg-zinc-300 p-4 mb-2 rounded-md"
+      onClick={() => onResourceClick(resource)}
+    >
+      <span className="text-lg font-semibold">{resource.name}</span>
+    </div>
+  ))}
+</div>
+
+
+  {/* Right Details Pane (2/3 of the window) */}
+  <div className="w-2/3 h-full bg-white p-6 rounded-md ml-4 mt-8"> 
+      <button onClick={onClose} className="rounded py-1 px-3 absolute right-4 top-4">
+        <XMarkIcon className="h-6 w-6" />
+      </button>
+      {selectedResource && (
+        <>
+          <h2 className="text-lg font-semibold mb-4">
+            {selectedResource
+              ? ` ${selectedResource.name}'s Details`
+              : 'Resource Name'}
+          </h2>
+          <table className="min-w-full table-auto">
+  <thead className="">
+    <tr className="text-zinc-400 font-normal text-left">
+      <th className="p-2 font-normal text-sm">Project</th>
+      <th className="p-2 font-normal text-sm">Allocated Date</th>
+      <th className="p-2 font-normal text-sm">Release Date</th>
+      <th className="p-2 font-normal text-sm">Status</th>
+      <th className="p-2 font-normal text-sm">Percentage</th>
+    </tr>
+  </thead>
+  <tbody className="border-y border-gray-300 text-sm">
+    {selectedResource && selectedResource.details.map((detail, projectIndex) => (
+      <tr key={projectIndex}>
+        <td className="border-b p-2 font-semibold">{selectedResource.allocatedProjects[projectIndex]?.name}</td>
+        <td className="border-b p-2">{detail.allocatedDate.toLocaleDateString()}</td>
+        <td className="border-b p-2">{detail.releaseDate.toLocaleDateString()}</td>
+        <td className="border-b p-2">{selectedResource.status}</td>
+        <td className="border-b p-2">{detail.contributionPercentage.toFixed(2)}%</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+
+    </>
+  )}
 </div>
 
         </div>
-
-      
       </div>
     </div>
-  );
+ );
 };
 
 export default RequestDialog;
--- src/pages/resource-manager/resources-manager-page.tsx
+++ src/pages/resource-manager/resources-manager-page.tsx
@@ -10,8 +10,6 @@ import React from "react";
 import WorkPerecentageCurrent from "./Componants/work-perecentage-current";
 import { Button } from '@mui/material';
 import FilterPopup from "./Componants/filter";
-import Filter from "./Componants/filter";
-import FilterButton from "./Componants/filter";
 
 
 type Filter = {
@@ -194,11 +192,13 @@ export function ResourcesManagerPage() {
             />
 
             <div>
-             
+              <button className="bg-zinc-200  rounded-md px-3 text-xs py-1.5 mr-1 flex items-center" onClick={openPopup}>
+                <AdjustmentsHorizontalIcon className="w-4 h-4 mr-2" />
+                Filter
+              </button>
 
-              <FilterButton/>
-
-              {/* Display existing filters */}
+              {/* <FilterPopup isOpen={isPopupOpen} onClose={closePopup} onAddFilter={handleAddFilter} />
+             Display existing filters */}
               <div>
                 {filters.map((filter, index) => (
                   <div key={index}>
